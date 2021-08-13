8725e1374ac923503b367274c46d3884fe1aba48
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-08-13 13:34:09 +0300

    Create Web Project

---
+++ Taskly/01.Taskly.Web/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Taskly.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
