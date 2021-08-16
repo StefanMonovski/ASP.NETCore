f9d3fe6a14265fc612e60af41b81f6eb221e2601
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-08-16 19:34:30 +0300

    Add AutoMapper

---
+++ Taskly/03.Taskly.Services/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Taskly.Data.Models;
+using Taskly.Services.DataTransferObjects;
+
+namespace Taskly.Services.Mapping
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Label, LabelDto>();
+            CreateMap<Note, NoteDto>();
+            CreateMap<Priority, PriorityDto>();
+            CreateMap<Comment, CommentDto>();
+            CreateMap<CommentReaction, CommentReactionDto>();
+            CreateMap<Reply, ReplyDto>();
+            CreateMap<ReplyReaction, ReplyReactionDto>();
+            CreateMap<Project, ProjectDto>();
+            CreateMap<ProjectUser, ProjectUserDto>();
+            CreateMap<Task, TaskDto>();
+        }
+    }
+}
