74bb210fc21194321c2028ad567e50ea34b1edc7
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-08-31 11:44:45 +0300

    Create Projects Controller

---
+++ Taskly/01.Taskly.Web/Controllers/ProjectsController.cs
@@ -0,0 +1,70 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Taskly.Services.Interfaces;
+using Taskly.Web.ViewModels;
+
+namespace Taskly.Web.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private readonly IProjectService projectService;
+        private readonly IMapper mapper;
+
+        public ProjectsController(IProjectService projectService, IMapper mapper)
+        {
+            this.projectService = projectService;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult All()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var projects = projectService.GetAllProjects(userId);
+            var projectsViewModel = mapper.Map<List<ProjectsViewModel>>(projects);
+
+            return View(projectsViewModel);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Personal()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var projects = projectService.GetAllPersonalProjects(userId);
+            var projectsViewModel = mapper.Map<List<ProjectsViewModel>>(projects);
+
+            return View(projectsViewModel);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Favorite()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var projects = projectService.GetAllFavoriteProjects(userId);
+            var projectsViewModel = mapper.Map<List<ProjectsViewModel>>(projects);
+
+            return View(projectsViewModel);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Collaborative()
+        {
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            var projects = projectService.GetAllCollaborativeProjects(userId);
+            var projectsViewModel = mapper.Map<List<ProjectsViewModel>>(projects);
+
+            return View(projectsViewModel);
+        }
+    }
+}
