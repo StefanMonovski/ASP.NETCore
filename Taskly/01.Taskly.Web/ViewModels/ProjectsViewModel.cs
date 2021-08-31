65b574f2ea8332455c7562313a220a1a6a69521a
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-08-31 11:54:30 +0300

    Create Projects Views

---
+++ Taskly/01.Taskly.Web/ViewModels/ProjectsViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Taskly.Web.ViewModels
+{
+    public class ProjectsViewModel
+    {
+        public string Guid { get; set; }
+
+        public string Title { get; set; }
+
+        public string ColorHex { get; set; }
+
+        public bool IsPersonal { get; set; }
+
+        public bool IsFavorite { get; set; }
+    }
+}
