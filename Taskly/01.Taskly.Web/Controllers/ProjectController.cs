88cc6d65876e8a22dcace250ada28e07a0eaf984
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-09-07 18:10:14 +0300

    Create Modal View Project Input

---
+++ Taskly/01.Taskly.Web/Controllers/ProjectController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Taskly.Services.Interfaces;
+using Taskly.Web.InputModels;
+
+namespace Taskly.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            this.projectService = projectService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Add(ProjectInputModel inputModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string guid = await projectService.AddProjectAsync(inputModel.Title, inputModel.ColorPicker, userId);
+
+            return RedirectToAction("Current", new { guid });
+        }
+    }
+}
---
+++ Taskly/01.Taskly.Web/InputModels/ProjectInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskly.Web.InputModels
+{
+    public class ProjectInputModel
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(100, ErrorMessage = "Title must be at max 100 characters long.")]
+        public string Title { get; set; }
+
+        [Required]
+        public string ColorPicker { get; set; }
+    }
+}
