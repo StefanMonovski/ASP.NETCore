f44f9718c9bc89adf4635af5d4497402d3075e42
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-09-07 17:54:53 +0300

    Add Web AutoMapper

---
+++ Taskly/01.Taskly.Web/Mapping/WebAutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Taskly.Services.DataTransferObjects;
+using Taskly.Web.ViewModels;
+
+namespace Taskly.Web.Mapping
+{
+    public class WebAutoMapperProfile : Profile
+    {
+        public WebAutoMapperProfile()
+        {
+            CreateMap<ProjectDto, ProjectsViewModel>();
+            CreateMap<ProjectDto, ProjectViewModel>();
+        }
+    }
+}
