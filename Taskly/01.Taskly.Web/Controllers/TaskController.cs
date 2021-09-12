2a701d44b625368f42d94b4336789e4938ecec85
Stefan Monovski <71312424+StefanMonovski@users.noreply.github.com>
2021-09-12 17:58:54 +0300

    Create Modal View Task Input

---
+++ Taskly/01.Taskly.Web/Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Taskly.Services.Interfaces;
+using Taskly.Web.InputModels;
+
+namespace Taskly.Web.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskService taskService;
+
+        public TaskController(ITaskService taskService)
+        {
+            this.taskService = taskService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Add(TaskInputModel inputModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await taskService.AddTaskToProjectAsync(inputModel.Title, inputModel.Description, inputModel.ProjectId, userId);
+
+            return RedirectToAction("Current", "Project" , new { guid = inputModel.ProjectGuid });
+        }
+    }
+}
---
+++ Taskly/01.Taskly.Web/InputModels/TaskInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taskly.Web.InputModels
+{
+    public class TaskInputModel
+    {
+        public int ProjectId { get; set; }
+
+        public string ProjectGuid { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        [MaxLength(20, ErrorMessage = "Title must be at max 20 characters long.")]
+        public string Title { get; set; }
+
+        [MaxLength(500, ErrorMessage = "Description must be at max 500 characters long.")]
+        public string Description { get; set; }
+    }
+}
